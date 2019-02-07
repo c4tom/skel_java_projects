1f5f8b4c4da18ff20ee74b2131ce2a041c553eed
Candido Tominaga <c049262@copel.nt>
2019-02-07 16:10:37 -0200

    Projeto minimo:
    Maven, primefaces 6 (bootstrap, extensoes), jboss, jsp

---
+++ mvn-jsp-jsf2-primefaces6/src/main/java/com/view/MainController.java
@@ -0,0 +1,41 @@
+package com.view;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainController implements Serializable {
+
+	private static final long serialVersionUID = 3971801993975443027L;
+
+	private String name;
+	private String middleName;
+	private String lastName;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ mvn-jsp-jsf2-primefaces6/src/main/webapp/about.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@page import="com.view.MainController"%>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta
+	http-equiv="Content-Type"
+	content="text/html; charset=iso-8859-1" />
+<title>Titulo</title>
+
+<script type="text/javascript">
+</script>
+</head>
+<%
+response.sendRedirect("index.jsf");
+%>
+
+<body>Olá mundo
+</body>
+</html>
